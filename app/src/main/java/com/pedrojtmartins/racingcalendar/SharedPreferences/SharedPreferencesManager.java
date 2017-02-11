274bf83e2ac3700ee54723b08f95bf14412bc685
Pedro Martins <pedrojtmartins@gmail.com>
2017-02-11 23:54:24 +0000

    Add favourites to shared preferences

--- app/src/main/java/com/pedrojtmartins/racingcalendar/SharedPreferences/SharedPreferencesManager.java
+++ app/src/main/java/com/pedrojtmartins/racingcalendar/SharedPreferences/SharedPreferencesManager.java
@@ -13,6 +13,7 @@ public class SharedPreferencesManager {
 
     private final String SHARED_PREFS = "sharedPrefs";
     private final String DATA_VERSION = "dataVersion";
+    private final String FAVOURITE_IDS = "favourites";
 
     public SharedPreferencesManager(Context context) {
         mContext = context;
@@ -26,6 +27,24 @@ public class SharedPreferencesManager {
         SharedPreferences sp = mContext.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         editor.putInt(DATA_VERSION, version);
+        editor.apply();
+    }
+
+    public String getFavouriteIds() {
+        SharedPreferences sp = mContext.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        return sp.getString(FAVOURITE_IDS, "");
+    }
+
+    public void addFavouriteIds(String arrFavs) {
+        SharedPreferences sp = mContext.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        if (arrFavs == null || arrFavs.length() == 0) {
+            editor.remove(FAVOURITE_IDS);
+        } else {
+            editor.putString(FAVOURITE_IDS, arrFavs);
+        }
+
         editor.apply();
     }
 }
