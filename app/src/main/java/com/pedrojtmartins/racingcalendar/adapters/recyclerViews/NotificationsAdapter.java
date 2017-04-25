8087f16aa79d8671489f12408ab42a9deb5a0ca3
Pedro Martins <pedrojtmartins@gmail.com>
2017-04-25 16:47:24 +0100

    Improve support for older apis (tinting <21)

--- app/src/main/java/com/pedrojtmartins/racingcalendar/adapters/recyclerViews/NotificationsAdapter.java
+++ app/src/main/java/com/pedrojtmartins/racingcalendar/adapters/recyclerViews/NotificationsAdapter.java
@@ -1,9 +1,13 @@
 package com.pedrojtmartins.racingcalendar.adapters.recyclerViews;
 
 import android.databinding.ObservableArrayList;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatImageView;
 import android.view.View;
 
 import com.pedrojtmartins.racingcalendar.BR;
+import com.pedrojtmartins.racingcalendar.R;
 import com.pedrojtmartins.racingcalendar.interfaces.fragments.INotificationsCallback;
 import com.pedrojtmartins.racingcalendar.models.RCNotification;
 
@@ -22,10 +26,13 @@ public class NotificationsAdapter extends ObservableAdapter<RCNotification> {
 
 
     @Override
-    public void onBindViewHolder(ObservableAdapter.ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(final ObservableAdapter.ViewHolder viewHolder, int position) {
         if (mValues != null && mValues.size() > position) {
             final RCNotification currNotification = mValues.get(position);
             viewHolder.mDataBinding.setVariable(BR.data, currNotification);
+
+            setTintForOlderApis(viewHolder.mDataBinding.getRoot(), currNotification.isToDelete(), viewHolder);
+
 
             viewHolder.mDataBinding.getRoot().setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -33,9 +40,26 @@ public class NotificationsAdapter extends ObservableAdapter<RCNotification> {
                     if (mCallback != null) {
                         boolean res = mCallback.deleteNotification(currNotification);
                         updateStatus(currNotification, res);
+
+                        setTintForOlderApis(view, res, viewHolder);
                     }
                 }
             });
+        }
+    }
+
+    private void setTintForOlderApis(View view, boolean res, ViewHolder viewHolder) {
+        if (Build.VERSION.SDK_INT < 21) {
+            View v = viewHolder.mDataBinding.getRoot().findViewById(R.id.notification_imv);
+            if (v == null)
+                return;
+
+            AppCompatImageView imv = (AppCompatImageView) v;
+            if (res) {
+                imv.setColorFilter(ContextCompat.getColor(view.getContext(), R.color.tintDisabled));
+            } else {
+                imv.setColorFilter(ContextCompat.getColor(view.getContext(), R.color.tintEnabled));
+            }
         }
     }
 
--- app/src/main/java/com/pedrojtmartins/racingcalendar/adapters/recyclerViews/SeriesSelectionAdapter.java
+++ app/src/main/java/com/pedrojtmartins/racingcalendar/adapters/recyclerViews/SeriesSelectionAdapter.java
@@ -1,9 +1,13 @@
 package com.pedrojtmartins.racingcalendar.adapters.recyclerViews;
 
 import android.databinding.ObservableArrayList;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatImageView;
 import android.view.View;
 
 import com.pedrojtmartins.racingcalendar.BR;
+import com.pedrojtmartins.racingcalendar.R;
 import com.pedrojtmartins.racingcalendar.models.Series;
 
 /**
@@ -17,18 +21,37 @@ public class SeriesSelectionAdapter extends ObservableAdapter<Series> {
     }
 
     @Override
-    public void onBindViewHolder(ObservableAdapter.ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(final ObservableAdapter.ViewHolder viewHolder, int position) {
         if (mValues != null && mValues.size() > position) {
             final Series currSeries = mValues.get(position);
             viewHolder.mDataBinding.setVariable(BR.data, currSeries);
 
+            setTintForOlderApis(viewHolder.mDataBinding.getRoot(), currSeries.isFavorite(), viewHolder);
+
             viewHolder.mDataBinding.getRoot().setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
+                public void onClick(View view) {
                     boolean newState = !currSeries.isFavorite();
                     currSeries.setFavorite(newState);
+
+                    setTintForOlderApis(view, newState, viewHolder);
                 }
             });
+        }
+    }
+
+    private void setTintForOlderApis(View view, boolean newState, ViewHolder viewHolder) {
+        if (Build.VERSION.SDK_INT < 21) {
+            View v = viewHolder.mDataBinding.getRoot().findViewById(R.id.favourites_imv);
+            if (v == null)
+                return;
+
+            AppCompatImageView imv = (AppCompatImageView) v;
+            if (newState) {
+                imv.setColorFilter(ContextCompat.getColor(view.getContext(), R.color.tintEnabled));
+            } else {
+                imv.setColorFilter(ContextCompat.getColor(view.getContext(), R.color.tintDisabled));
+            }
         }
     }
 }
