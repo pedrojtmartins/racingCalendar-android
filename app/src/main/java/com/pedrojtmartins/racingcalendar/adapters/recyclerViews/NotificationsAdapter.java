c01f48133819c7963a9287385b7117d83f8c7ca0
Pedro Martins <pedrojtmartins@gmail.com>
2017-04-29 16:29:01 +0100

    Add notifications parameters update

--- app/src/main/java/com/pedrojtmartins/racingcalendar/adapters/recyclerViews/NotificationsAdapter.java
+++ app/src/main/java/com/pedrojtmartins/racingcalendar/adapters/recyclerViews/NotificationsAdapter.java
@@ -1,16 +1,22 @@
 package com.pedrojtmartins.racingcalendar.adapters.recyclerViews;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.databinding.ObservableArrayList;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatImageView;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import com.pedrojtmartins.racingcalendar.BR;
 import com.pedrojtmartins.racingcalendar.R;
+import com.pedrojtmartins.racingcalendar.alertDialog.AlertDialogHelper;
 import com.pedrojtmartins.racingcalendar.databinding.RowNotificationBinding;
 import com.pedrojtmartins.racingcalendar.helpers.APIHelper;
+import com.pedrojtmartins.racingcalendar.helpers.ParsingHelper;
 import com.pedrojtmartins.racingcalendar.interfaces.fragments.INotificationsCallback;
 import com.pedrojtmartins.racingcalendar.models.RCNotification;
 
@@ -48,7 +54,7 @@ public class NotificationsAdapter extends ObservableAdapter<RCNotification> {
                 @Override
                 public void onClick(View view) {
                     if (mCallback != null) {
-                        boolean res = mCallback.deleteNotification(currNotification);
+                        boolean res = mCallback.updateNotification(currNotification, true);
                         updateStatus(currNotification, res);
 
                         setTintForOlderApis(res, viewHolder);
@@ -61,10 +67,26 @@ public class NotificationsAdapter extends ObservableAdapter<RCNotification> {
                 binding.notificationEditTimeBefore.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-//                        if (mCallback != null) {
-//                            mCallback.openNotificationsActivity(currNotification);
-//                        }
-                        // TODO: 29/04/2017 implement
+                        Context context = viewHolder.mDataBinding.getRoot().getContext();
+                        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                        AlertDialogHelper.displayNewNotificationDialog(
+                                context,
+                                layoutInflater,
+                                currNotification.minutesBefore + "",
+                                false,
+                                new Handler(new Handler.Callback() {
+                                    @Override
+                                    public boolean handleMessage(Message msg) {
+                                        String timeBefore = msg.getData().getString("timeBefore");
+                                        if (timeBefore == null || timeBefore.isEmpty())
+                                            return true;
+
+                                        currNotification.setTimeBefore(ParsingHelper.stringToInt(timeBefore));
+                                        boolean res = mCallback.updateNotification(currNotification, false);
+
+                                        return true;
+                                    }
+                                }));
                     }
                 });
             }
@@ -110,4 +132,5 @@ public class NotificationsAdapter extends ObservableAdapter<RCNotification> {
     private void updateStatus(RCNotification notif, boolean res) {
         notif.setToDelete(res);
     }
+
 }
